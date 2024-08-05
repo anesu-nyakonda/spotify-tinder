755552db03a52dda55e143c624c400e481f06fce
anesu nyakonda <nyakondaa@africau.edu>
2024-08-05 11:18:40 +0200

    getting access token successfully

--- backend/server.js
+++ backend/server.js
@@ -1,59 +1,57 @@
 const express = require("express");
-
+const { get } = require("http");
+const querystring = require("querystring");
 const app = express();
 const redirect_uri = "http://localhost:3000/callback";
 const url = "https://accounts.spotify.com/authorize?";
-let code = null;
+let code = undefined;
+const params = new URLSearchParams();
 
-//if the user has authorized
+if (code == undefined) {
+  redirectAuthflow(process.env.CLIENT_ID);
+}
 
-redirectToAuth = async () => {
-  const params = await new URLSearchParams();
-  params.append("response_type", "code");
-  params.append("client_id", process.env.client_id);
-  params.append("redirect_uri", redirect_uri);
-
+async function redirectAuthflow(client_id) {
   app.get("/login", (req, res) => {
-    res.redirect(url + `${params.toString()}`);
+    res.redirect(
+      "https://accounts.spotify.com/authorize?" +
+        querystring.stringify({
+          response_type: "code",
+          client_id: client_id,
+          redirect_uri: redirect_uri,
+        })
+    );
   });
-};
 
-getAuthcode = async () => {
   app.get("/callback", (req, res) => {
     code = req.query.code;
-    res.send("nice");
+    if (code != undefined) {
+      getAccessToken(code, process.env.CLIENT_ID, process.env.CLIENT_SECRET);
+    }
+
+    res.send("hey");
   });
-};
+}
 
-getAccess_token = async (code) => {
-  const auth_options = {
-    grant_type: "authorization_code",
-    code: code,
-    redirect_uri: redirect_uri,
-  };
-
-  console.log("hey");
-  let response = await fetch("https://accounts.spotify.com/api/token", {
+async function getAccessToken(code, client_id, client_secret) {
+  const response = await fetch("https://accounts.spotify.com/api/token", {
     method: "POST",
-    body: JSON.stringify(auth_options),
+    body: new URLSearchParams({
+      code: code,
+      redirect_uri: redirect_uri,
+      grant_type: "authorization_code",
+    }),
     headers: {
       "content-type": "application/x-www-form-urlencoded",
       Authorization:
         "Basic " +
-        new Buffer.from(
-          process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET
-        ).toString("base64"),
+        new Buffer.from(client_id + ":" + client_secret).toString("base64"),
     },
+    json: true,
   });
 
-  console.log(response.json());
-};
-
-if (code == null) {
-  redirectToAuth();
-} else {
-  getAuthcode();
-  getAccess_token(code);
+  const result = await response.json();
+  console.log(result.access_token);
 }
 
 app.listen(3000, () => {
